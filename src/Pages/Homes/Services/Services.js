598f3d96abd10a0ae618c8fbdb8952155fe7f308
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-04-18 02:06:06 +0600

    make service section

--- src/Pages/Homes/Services/Services.js
+++ src/Pages/Homes/Services/Services.js
@@ -3,9 +3,9 @@ import SingleService from "../SingleService/SingleService";
 
 const Services = ({ services }) => {
   return (
-    <section className="container my-3">
-      <h2 className="text-center">Our Services</h2>
-      <div className="row">
+    <section className="container my-4">
+      <h2 className="text-center mb-3">Our Services</h2>
+      <div className="row my-3 g-3">
         {services.map((service) => (
           <SingleService key={service.id} props={service} />
         ))}
--- src/Pages/Homes/SingleService/SingleService.js
+++ src/Pages/Homes/SingleService/SingleService.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const SingleService = ({ props }) => {
-  console.log(props);
-  const { id, desc, name, price, img } = props;
+  const { desc, name, price, img } = props;
   return (
     <div className="col-12 col-sm-6 col-lg-4">
-      <h2>single service</h2>
+      <Card>
+        <Card.Img
+          variant="top"
+          src={img}
+          alt={name}
+          className="w-100 rounded-top"
+        />
+        <Card.Body>
+          <Card.Title className="fs-3">{name}</Card.Title>
+          <Card.Text style={{ minHeight: "120px" }}>
+            {desc.slice(0, 150)} ...
+          </Card.Text>
+          <Card.Title>${price}</Card.Title>
+        </Card.Body>
+        <Card.Footer>
+          <button className="btn btn-primary">Book Now</button>
+        </Card.Footer>
+      </Card>
     </div>
   );
 };
